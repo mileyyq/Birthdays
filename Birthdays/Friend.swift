9c83aea9af7413f2d6dc6d311707cf6dec148e95
Miley <cutegoldendragon@gmail.com>
2025-07-25 10:22:25 -0400

    coded borthdays

---
+++ Birthdays/Friend.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class Friend{
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date){
+        self.name = name
+        self.birthday = birthday
+    }
+    
+}
